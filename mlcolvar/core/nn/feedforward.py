48b61ad15cf12e02c966ceb753ba04352808603f
jintuzhang <119382168+jintuzhang@users.noreply.github.com>
2024-08-05 23:28:42 +0200

    Initial attemption.

--- mlcolvar/core/nn/feedforward.py
+++ mlcolvar/core/nn/feedforward.py
@@ -19,7 +19,8 @@ from typing import Optional, Union
 
 import torch
 import lightning
-from mlcolvar.core.nn.utils import get_activation, parse_nn_options
+import functools
+from mlcolvar.core.nn.utils import get_activation, parse_nn_options, QLinear
 
 
 # =============================================================================
@@ -40,6 +41,7 @@ class FeedForward(lightning.LightningModule):
         dropout: Optional[Union[float, list]] = None,
         batchnorm: Union[bool, list] = False,
         last_layer_activation: bool = False,
+        quantization: bool = False,
         **kwargs,
     ):
         """Constructor.
@@ -66,6 +68,8 @@ class FeedForward(lightning.LightningModule):
             (i.e., they are not lists). Otherwise, the output layer will be linear.
             This option is ignored for the arguments among ``activation``, ``dropout``,
             and ``batchnorm`` that are passed as lists.
+        quantization : bool, optional
+            If enable quantization-aware training.
         **kwargs:
             Optional arguments passed to torch.nn.Module
         """
@@ -83,11 +87,17 @@ class FeedForward(lightning.LightningModule):
         dropout_list = parse_nn_options(dropout, n_layers, last_layer_activation)
         # -- batchnorm
         batchnorm_list = parse_nn_options(batchnorm, n_layers, last_layer_activation)
+        # -- linear layer
+        if quantization:
+            linear = QLinear
+        else:
+            linear = torch.nn.Linear
+        self.quantization = quantization
 
         # Create network
         modules = []
         for i in range(len(layers) - 1):
-            modules.append(torch.nn.Linear(layers[i], layers[i + 1]))
+            modules.append(linear(layers[i], layers[i + 1]))
             activ, drop, norm = activation_list[i], dropout_list[i], batchnorm_list[i]
 
             if activ is not None:
@@ -104,9 +114,74 @@ class FeedForward(lightning.LightningModule):
         self.in_features = layers[0]
         self.out_features = layers[-1]
 
+    def strip(self) -> None:
+        if not self.quantization:
+            return
+
+        indices = [
+            i for i in range(len(self.nn)) if isinstance(self.nn[i], QLinear)
+        ]
+        # strip with a reversed order
+        indices = list(reversed(indices))
+
+        m = self.nn[indices[0]]
+        m = m.strip(self.nn[indices[1]].alive_weight_indices, 1)
+        self.nn[indices[0]] = m
+
+        for i in range(1, len(indices) - 1):
+            m = self.nn[indices[i]]
+            m = m.strip(m.alive_weight_indices, 0)
+            m = m.strip(self.nn[indices[i + 1]].alive_weight_indices, 1)
+            self.nn[indices[i]] = m
+
+        m = self.nn[indices[-1]]
+        m = m.strip(m.alive_weight_indices, 0)
+        self.nn[indices[-1]] = m
+
+    def freeze(self) -> None:
+        if not self.quantization:
+            return
+
+        indices = [
+            i for i in range(len(self.nn)) if isinstance(self.nn[i], QLinear)
+        ]
+
+        for i in range(0, len(indices)):
+            m = self.nn[indices[i]]
+            self.nn[indices[i]] = m.freeze()
+
     # def extra_repr(self) -> str:
     #    repr = f"in_features={self.in_features}, out_features={self.out_features}"
     #    return repr
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.nn(x)
+
+    @property
+    def qbits_mean(self) -> torch.Tensor:
+        if self.quantization:
+            q = sum([
+                m.qbits for m in self.nn if isinstance(m, QLinear)
+            ])
+            return q / self.n_weights
+        else:
+            return torch.zeros(1)
+
+    @property
+    def n_weights(self) -> torch.Tensor:
+        if self.quantization:
+            linear = QLinear
+        else:
+            linear = torch.nn.Linear
+        return sum([
+            m.weight.nelement() for m in self.nn if isinstance(m, linear)
+        ])
+
+    @property
+    def n_weights_alive(self) -> torch.Tensor:
+        if self.quantization:
+            return sum([
+                m.n_weights_alive for m in self.nn if isinstance(m, QLinear)
+            ])
+        else:
+            return self.n_weights
--- mlcolvar/core/nn/utils.py
+++ mlcolvar/core/nn/utils.py
@@ -1,6 +1,165 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import math
+
+
+class QLinear(nn.Module):
+    """
+    Linear transformation with quantization support [1]. Implementation was
+    stolen from geohot's notebooks [2].
+
+    Parameters
+    ----------
+    in_features: int
+        Size of each input sample.
+    out_features: int
+        Size of each output sample.
+    bias: bool
+        If set to ``False``, the layer will not learn an additive bias.
+
+    References
+    ----------
+    .. [1] Self-Compressing Neural Networks. (https://arxiv.org/pdf/2301.13142)
+    .. [2] https://github.com/geohot/ai-notebooks/blob/master/mnist_self_compression.ipynb
+    """
+
+    __constants__ = ['in_features', 'out_features']
+    in_features: int
+    out_features: int
+    weight: torch.Tensor
+    e: torch.Tensor
+    b: torch.Tensor
+
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool = True,
+        device=None,
+        dtype=None,
+    ) -> None:
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = nn.parameter.Parameter(
+            torch.empty((out_features, in_features), **factory_kwargs)
+        )
+        self.e = nn.parameter.Parameter(
+            torch.full((out_features, 1), -4.0, **factory_kwargs)
+        )
+        self.b = nn.parameter.Parameter(
+            torch.full((out_features, 1), 3.0, **factory_kwargs)
+        )
+        if bias:
+            self.bias = nn.parameter.Parameter(
+                torch.empty(out_features, **factory_kwargs)
+            )
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        qw = self.qweight
+        w = (qw.round() - qw).detach() + qw  # straight through estimator
+        w = 2 ** self.e * w
+        return nn.functional.linear(input, w, self.bias)
+
+    def extra_repr(self) -> str:
+        return (
+            f'in_features={self.in_features}), '
+            + f'out_features={self.out_features}, '
+            + f' bias={self.bias is not None}'
+        )
+
+    def strip(self, indices: torch.Tensor, dim: int) -> 'QLinear':
+        if dim not in [0, 1]:
+            raise IndexError('dim should be 0 or 1!')
+
+        # strip in channels
+        if dim == 1:
+            result = QLinear(
+                len(indices),
+                self.out_features,
+                self.bias is not None,
+                self.weight.device,
+                self.weight.dtype
+            )
+            with torch.no_grad():
+                result.weight.copy_(self.weight[:, indices])
+                if self.bias is not None:
+                    result.bias.copy_(self.bias)
+                result.e.copy_(self.e)
+                result.b.copy_(self.b)
+
+        # strip out channels
+        if dim == 0:
+            result = QLinear(
+                self.in_features,
+                len(indices),
+                self.bias is not None,
+                self.weight.device,
+                self.weight.dtype
+            )
+            with torch.no_grad():
+                result.weight.copy_(self.weight[indices, :])
+                if self.bias is not None:
+                    result.bias.copy_(self.bias[indices])
+                result.e.copy_(self.e[indices])
+                result.b.copy_(self.b[indices])
+
+        result = result.train(self.training)
+
+        return result
+
+    def freeze(self) -> nn.Linear:
+        result = nn.Linear(
+            self.in_features,
+            self.out_features,
+            self.bias is not None,
+            self.weight.device,
+            self.weight.dtype
+        )
+
+        with torch.no_grad():
+            qw = self.qweight
+            w = (qw.round() - qw).detach() + qw
+            w = 2 ** self.e * w
+            result.weight.copy_(w)
+            if self.bias is not None:
+                result.bias.copy_(self.bias)
+
+        result = result.train(self.training)
+
+        return result
+
+    @property
+    def n_weights_alive(self) -> torch.Tensor:
+        return len(torch.nonzero(self.b.relu())) * self.weight.shape[1]
+
+    @property
+    def alive_weight_indices(self) -> torch.Tensor:
+        return torch.nonzero(self.b.relu())[:, 0]
+
+    @property
+    def qbits(self) -> torch.Tensor:
+        return self.b.relu().sum() * self.weight.shape[1]
+
+    @property
+    def qweight(self) -> torch.Tensor:
+        b_plus = self.b.relu() - 1
+        return torch.min(
+            torch.max(2 ** -self.e * self.weight, -2 ** b_plus),
+            2 ** b_plus - 1
+        )
 
 
 class Shifted_Softplus(torch.nn.Softplus):
@@ -12,7 +171,8 @@ class Shifted_Softplus(torch.nn.Softplus):
     def forward(self, input):
         sp0 = F.softplus(torch.zeros(1), self.beta, self.threshold).item()
         return F.softplus(input, self.beta, self.threshold) - sp0
-    
+
+
 class Custom_Sigmoid(torch.nn.Module):
     def __init__(self, p=3):
         super(Custom_Sigmoid, self).__init__()
@@ -36,7 +196,7 @@ def get_activation(activation: str):
     elif activation == "shifted_softplus":
         activ = Shifted_Softplus()
     elif activation == "custom_sigmoid":
-        activ = Custom_Sigmoid()    
+        activ = Custom_Sigmoid()
     elif activation == "linear":
         print("WARNING: no activation selected")
     elif activation is None:
