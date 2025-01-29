f148044bf50a187e354a29f77bfa175c09dcd657
jintuzhang <119382168+jintuzhang@users.noreply.github.com>
2025-01-29 16:47:44 +0100

    Added the time-lagged committor (in testing).

--- mlcolvar/graph/cvs/committor/__init__.py
+++ mlcolvar/graph/cvs/committor/__init__.py
@@ -1,3 +1,4 @@
 from . import utils
 from .utils import get_dataset_kolmogorov_bias, compute_committor_weights
 from .committor import GraphCommittor
+from .tlcommittor import GraphTimeLaggedCommittor
